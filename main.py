0dc8cff747509dcea5022d3f93e8511362cbe90c
Juan-789 <79994699+Juan-789@users.noreply.github.com>
2021-03-09 11:57:09 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,8 @@
+let Boolean = false
+input.onButtonPressed(Button.A, function () {
+    Boolean = true || true
+    basic.showString("" + (Boolean))
+})
+basic.forever(function () {
+	
+})
---
+++ main.ts
@@ -0,0 +1,15 @@
+let Boolean = false
+let coin_1 = false
+input.onButtonPressed(Button.A, function () {
+    Boolean = 3 != 4
+    basic.showString("" + (Boolean))
+})
+input.onButtonPressed(Button.B, function () {
+    coin_1 = Math.randomBoolean()
+    if (coin_1 == true || false) {
+    	
+    }
+})
+basic.forever(function () {
+	
+})
